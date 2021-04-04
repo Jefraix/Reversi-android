262a29290932a3f28cf8937767c568e75678ac14
Juan <jaguiler@gmail.com>
2021-04-03 20:23:04 -0400

    Started designing layout for fragments. Layouts are not yet complete.

--- app/src/main/java/com/jaguiler/reversi/ui/main/TitleFragment.kt
+++ app/src/main/java/com/jaguiler/reversi/ui/main/TitleFragment.kt
@@ -5,13 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import com.jaguiler.reversi.R
 
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.title_fragment, container, false)
+    private lateinit var startButton: Button
+    private lateinit var settingsButton: Button
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val view =  inflater.inflate(R.layout.title_fragment, container, false)
+
+        startButton = view.findViewById(R.id.start_button)
+        settingsButton = view.findViewById(R.id.settings_button)
+
+        startButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_titleFragment_to_configFragment)
+        }
+        settingsButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_titleFragment_to_settingsFragment)
+        }
+
+        return view
     }
 
 }
